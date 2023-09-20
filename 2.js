5c6bde36c85f9caf32a96ceb02fd6dc710868a26
biia_santosss_ <133895171+BiaSantos01@users.noreply.github.com>
2023-09-20 13:53:42 -0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,15 @@
+function valor(){
+    var vl1 = parseInt(prompt('digite o primeiro valor'))
+    var vl2 = parseInt(prompt('digite o segundo valor'))
+    var vl3 = parseInt(prompt('digite o terceiro valor'))
+    var soma,sub;
+
+    soma=vl1,vl2,vl3;
+    sub=vl1,vl2,vl3;
+
+    if ((dia >= '' && mes >= '') && (dia <= '' && mes <= '')) {
+}
+    else if ((dia >= '' && mes >= '') && (dia <= '' && mes <= '')) {
+}
+
+}
